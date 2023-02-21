2e70273e88e3ebd7eddb4b7c02675261e36e3d0d
Wu Yujie <coffee377@dingtalk.com>
2023-02-21 17:57:15 +0800

    :zap: 日志组件

--- dolphinscheduler-ui/src/router/modules/security.ts
+++ dolphinscheduler-ui/src/router/modules/security.ts
@@ -43,8 +43,8 @@ export default {
     {
       path: '/security/log/task',
       name: 'task-log',
-      // component: components['monitor-statistics-audit-log'],
-      component: components['security-log-task'],
+      component: components['projects-task-instance'],
+      // component: components['security-log-task'],
       meta: {
         title: '任务日志',
         activeMenu: 'security',
@@ -55,8 +55,8 @@ export default {
     {
       path: '/security/log/audit',
       name: 'audit-log',
-      // component: components['monitor-statistics-audit-log'],
-      component: components['security-log-audit'],
+      component: components['monitor-statistics-audit-log'],
+      // component: components['security-log-audit'],
       meta: {
         title: '审计日志',
         activeMenu: 'security',
