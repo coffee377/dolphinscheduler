2ef209e5a53e3ced20f936999b1b4ac5b3f4242f
gaofan <gaofanis@163.com>
2023-03-24 09:43:23 +0800

    调整

--- integration-platform/src/main/java/net/jqsoft/integration/platform/mapper/DsxJobMapper.java
+++ integration-platform/src/main/java/net/jqsoft/integration/platform/mapper/DsxJobMapper.java
@@ -1,7 +1,6 @@
 package net.jqsoft.integration.platform.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import net.jqsoft.integration.platform.model.entity.DxsComponentType;
 import net.jqsoft.integration.platform.model.entity.DxsJob;
 import org.apache.ibatis.annotations.Mapper;
 
